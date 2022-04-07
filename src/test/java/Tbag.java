dff17e9e5fae07641a507ae95f3da82dfcb6057e
LK920 <969217312@qq.com>
2022-04-07 21:44:55 +0800

    第一次提交

---
+++ src/test/java/Tbag.java
@@ -0,0 +1,12 @@
+/**
+ * @program: test_maven
+ * @description:
+ * @author: Trans
+ * @create: 2022-04-07 21:41
+ **/
+public class Tbag {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
