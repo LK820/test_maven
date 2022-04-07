b55dc40ab0ba5db3dafd9813087c8e02fd3b6732
LK920 <969217312@qq.com>
2022-04-07 21:47:44 +0800

    第2次提交

--- src/test/java/Tbag.java
+++ src/test/java/Tbag.java
@@ -7,7 +7,8 @@
 public class Tbag {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 
 }
